8a97b8058928aecde876ebad78b326ae0eb9d97e
patriciakmadrid <125594817+patriciakmadrid@users.noreply.github.com>
2024-11-18 15:36:32 -0700

    BadAppleAuthoring.cs

---
+++ Scripts/BadAppleAuthoring.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+using UnityEngine;
+
+public struct BadAppleTag : IComponentData
+{
+}
+
+[DisallowMultipleComponent]
+public class BadAppleAuthoring : MonoBehaviour
+{
+    private class BadAppleAuthoringBaker : Baker<BadAppleAuthoring>
+    {
+        public override void Bake(BadAppleAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<BadAppleTag>(entity);
+        }
+    }
+}
